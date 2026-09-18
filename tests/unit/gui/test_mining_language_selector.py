b40416567a587c95e33349c232c17fbebd6da688
0xzerolight <205416739+0xzerolight@users.noreply.github.com>
2026-09-18 03:51:51 +0200

    feat(fi): ship the Finnish model as a language pack

--- tests/unit/gui/test_mining_language_selector.py
+++ tests/unit/gui/test_mining_language_selector.py
@@ -156,6 +156,7 @@ def test_the_panel_anchors_the_selector_and_every_pack_row(qtbot):
         "mining_language.language_pack_nb",
         "mining_language.language_pack_ro",
         "mining_language.language_pack_el",
+        "mining_language.language_pack_fi",
     }
 
 
